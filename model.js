31a2ce26e6fdac01d8ced03e7ff046b1058792d8
pmogan77 <60144163+pmogan77@users.noreply.github.com>
2021-03-14 21:10:23 -0500

    Initial deployment

---
+++ model.js
@@ -0,0 +1,121 @@
+import * as THREE from "/build/three.module.js";
+import { OrbitControls } from "/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "./jsm/loaders/GLTFLoader.js";
+
+var container = document.querySelector('.model');
+
+var height = 500;
+var width = 600;
+
+var controls, model;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+	75,
+	width / height,
+	0.5,
+	1000
+);
+
+camera.position.z = 10;
+
+var renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+
+renderer.setSize(width, height);
+
+container.appendChild(renderer.domElement);
+
+window.addEventListener('resize', function(){
+	var Width = container.clientWidth;
+    var Height = container.clientHeight;
+    renderer.setSize(Width, Height);
+    camera.aspect = Width / Height;
+    camera.updateProjectionMatrix();
+    controls.handleResize();
+})
+
+//entire object
+var hlight = new THREE.AmbientLight (0xd22222,100);
+scene.add(hlight);
+
+//top light
+var directionalLight = new THREE.DirectionalLight(0xdffffff,100);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+//top right hint
+var light = new THREE.PointLight(0xd22222,10);
+light.position.set(0,300,500);
+scene.add(light);
+
+//back right
+var light2 = new THREE.PointLight(0xd22222,10);
+light2.position.set(500,100,0);
+scene.add(light2);
+
+//back bottom
+var light3 = new THREE.PointLight(0xd22222,10);
+light3.position.set(0,100,-500);
+scene.add(light3);
+
+//top left hint
+var light4 = new THREE.PointLight(0xd22222,10);
+light4.position.set(-500,300,500);
+scene.add(light4);
+
+controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+controls.enableZoom = true;
+controls.enablePan = false;
+controls.enableRotate = true;
+
+
+const loader = new GLTFLoader();
+
+loader.load(
+	"./models/f35.gltf",
+	function (gltf) {
+		model = gltf.scene;
+
+		scene.add(gltf.scene);
+
+		renderer.setClearColor( 0x000000, 0 );
+
+		var newDir = new THREE.Vector3(-1, -.5, 1);
+
+		model.rotateOnWorldAxis(newDir, THREE.Math.degToRad(45));
+
+		var pos = new THREE.Vector3();
+		pos.addVectors(newDir, model.position);
+		model.lookAt(pos);
+
+		// var plane = new THREE.GridHelper(100, 10);
+  		// scene.add(plane);
+
+		//   var worldAxis = new THREE.AxesHelper(20);
+  		// model.add(worldAxis);
+
+		renderer.outputEncoding = THREE.sRGBEncoding
+
+		gameLoop();
+	},
+	undefined,
+	function (error) {
+		console.error(error);
+	}
+);
+
+function update(){
+	model.rotation.y += 0.01;
+	controls.update();
+}
+
+function render(){
+	renderer.render(scene,camera);
+}
+
+var gameLoop = function(){
+	requestAnimationFrame(gameLoop);
+	update();
+	render();
+}
---
+++ server.js
@@ -0,0 +1,279 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const url = require("url");
+
+var PORT = process.env.PORT || 3000;
+const startPath = `${__dirname}`;
+
+app.get(["/","/index.html", "/index"], (req,res)=>{
+
+    fs.readFile(startPath + '/index.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/404","/404.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/404.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/about","/about.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/about.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/contact","/contact.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/contact.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/projects","/projects.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/projects.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/skills","/skills.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/skills.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/eduportal","/eduportal.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/eduportal.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/store","/store.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/store.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/medical","/medical.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/medical.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/vocab","/vocab.html"], (req,res)=>{
+
+    fs.readFile(startPath + '/vocab.html', "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(["/covid","/covid.html"], (req,res)=>{
+
+      fs.readFile(startPath + '/covid.html', "utf-8", (err, data) => {
+            if (err) {
+                  console.log(err);
+                  res.redirect('/404');
+            }
+            else
+            {
+                  res.writeHead(200, { "Content-type": "text/html" });
+                  res.end(data);
+            }
+      }); 
+})
+
+app.get(["/test","/test.html"], (req,res)=>{
+
+      fs.readFile(startPath + '/test.html', "utf-8", (err, data) => {
+            if (err) {
+                  console.log(err);
+                  res.redirect('/404');
+            }
+            else
+            {
+                  res.writeHead(200, { "Content-type": "text/html" });
+                  res.end(data);
+            }
+      }); 
+})
+
+app.get([/\.(bin)$/i,/\.(gltf)$/i,/\.(glb)$/i], (req,res)=>{
+      fs.readFile("./" + req.originalUrl, (err, data) => {
+            if (err) {
+                  console.log(err);
+                  res.redirect('/404');
+            }
+            else
+            {
+                  res.writeHead(200, { "Content-type": "application/javascript" });
+                  res.end(data);
+            }
+      }); 
+})
+
+app.get(/\.(css)$/i, (req,res)=>{
+    fs.readFile(startPath + req.originalUrl, "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "text/css" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get(/\.(jpg|jpeg|png|gif|mp4|ogg)$/i, (req,res)=>{
+
+    console.log(req.originalUrl);
+
+    fs.readFile(startPath + req.originalUrl, (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else{
+                res.writeHead(200, { "Content-type": "image/png" });
+                res.end(data);
+          }
+    });
+})
+
+app.get(/\.(js)$/i, (req,res)=>{
+    fs.readFile(startPath + req.originalUrl, (err, data) => {
+          if (err||req.originalUrl === "/server.js") {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else{
+                res.writeHead(200, { "Content-type": "application/javascript" });
+                res.end(data);
+          }
+    });
+})
+
+app.get(/\.(json)$/i, (req,res)=>{
+    fs.readFile(startPath + req.originalUrl, "utf-8", (err, data) => {
+          if (err) {
+                console.log(err);
+                res.redirect('/404');
+          }
+          else
+          {
+                res.writeHead(200, { "Content-type": "application/json" });
+                res.end(data);
+          }
+    }); 
+})
+
+app.get('*', function(req, res){
+    res.writeHead(302, { Location: "404" });
+    res.end();
+});
+
+app.put('*', function(req, res){
+    res.end('No put pathway created.');
+});
+
+app.post('*', function(req, res){
+    res.end('No post pathway created.');
+});
+
+app.delete('*', function(req, res){
+    res.end('No delete pathway created.');
+});
+
+app.listen(PORT, () => {
+    console.log("Listening for reqests on port 3000.");
+});
